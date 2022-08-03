10eaedca0f485aa2a99063cc44338bdb6bf3a8bf
octavio <ruarteoctavio8@gmail.com>
2022-08-02 22:52:32 -0300

    maquetada seccion soy

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,16 @@
 async function main() {
     Header(document.querySelector(".home"));
 
-    const url = "https://cdn.contentful.com/spaces/4qwitj0i1jgx"
+    const url = "https://cdn.contentful.com/spaces/4qwitj0i1jgx/environments/master/entries?access_token=6t903P8lOC4T3JMvGR4qg1xCLkbmeicOKvUFbhNY95I"
     const res = await fetch(url, {
         headers: {
-            "content-type": "application/json",
-            "Authorization":"Bearer 6t903P8lOC4T3JMvGR4qg1xCLkbmeicOKvUFbhNY95I" }
+            "content-type": "application/json"
+        }
     });
 
-    console.log(res)
+    const json = await res.json()
+
+    console.log(json)
 }
 
 main()
