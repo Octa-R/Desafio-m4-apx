622c480ff520126cfde041fe91581c9e29127f2d
octavio <ruarteoctavio8@gmail.com>
2022-08-02 21:45:53 -0300

    agregado modal en el header

--- components/header.js
+++ components/header.js
@@ -12,15 +12,39 @@ function Header(el) {
           <a href="./portfolio.html">Portfolio</a>
         </li>
         <li class="header__links-link">
-          <a href="./services.html">Sevicios</a>
+          <a href="./services.html">Servicios</a>
         </li>
         <li class="header__links-link"> 
           <a href="./contact.html">Contacto</a>
         </li>
       </ul>
-      <img class="header__burguer-closed" src="./img/burger-closed.svg"></img>
+      <img class="header__burger-closed" src="./img/burger-closed.svg"></img>
+      <div class="header__window">
+        <img class="header__burger-open" src="./img/burguer-open.svg"></img>
+        <ul class="header__window-links">
+          <li class="header__window-link">
+            <a href="./portfolio.html">Portfolio</a>
+          </li>
+          <li class="header__window-link">
+            <a href="./services.html">Servicios</a>
+          </li>
+          <li class="header__window-link"> 
+            <a href="./contact.html">Contacto</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   `
+  function toggleVisibility(el) {
+    el.classList.toggle("visible")
+  }
+  const burgerClosed = headerEl.querySelector(".header__burger-closed");
+  const burgerOpen = headerEl.querySelector(".header__burger-open");
+  const headerWindow = headerEl.querySelector(".header__window");
+
+  burgerClosed.addEventListener("click",()=>toggleVisibility(headerWindow));
+  burgerOpen.addEventListener("click",()=>toggleVisibility(headerWindow));
+
   el.prepend(headerEl)
 }
 
